f64cba0246b0a35635e2a2977e2b36a8f25ace8f
Tamas Polgar <tms.polgar@gmail.com>
2016-07-17 00:01:00 +0100

    Finishing config

--- src/main/scala-2.11/poc/Config.scala
+++ src/main/scala-2.11/poc/Config.scala
@@ -3,6 +3,8 @@ package poc
 trait Config {
 
   def bootstrapServers = {
-
+    Option(System.getProperty("bootstrap.servers")).getOrElse {
+      throw new IllegalArgumentException("Environment variable bootstrap.servers is missing. Start the application with -Dbootstrap.servers=\"localhost:9092\"")
+    }
   }
 }
