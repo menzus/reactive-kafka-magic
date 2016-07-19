d4c6f37bca92cada3a5000bec0347a51a4a5b0d5
Tamas Polgar <tms.polgar@gmail.com>
2016-07-20 00:01:00 +0100

    Finishing consumer settings

--- src/main/scala-2.11/poc/ConsumerSettings.scala
+++ src/main/scala-2.11/poc/ConsumerSettings.scala
@@ -1,7 +1,14 @@
 package poc
 
 import akka.actor.ActorSystem
+import akka.kafka.{ConsumerSettings => KConsumerSettings}
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 
 trait ConsumerSettings {
-  def consumerSettings(groupId: String)(implicit system: ActorSystem) = {}
+  self: Config =>
+
+  def consumerSettings(groupId: String)(implicit system: ActorSystem) = KConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+    .withBootstrapServers(bootstrapServers)
+    .withClientId(groupId)
+    .withGroupId(groupId)
 }
