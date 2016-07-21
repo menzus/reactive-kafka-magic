4d5bb662ba543129a1fdb894d6466dd4432ec609
Tamas Polgar <tms.polgar@gmail.com>
2016-07-22 00:01:00 +0100

    Checking numbers

--- src/main/scala-2.11/poc/Outside.scala
+++ src/main/scala-2.11/poc/Outside.scala
@@ -1,5 +1,19 @@
 package poc
 
-class Outside {
+import akka.actor.ActorSystem
+import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
 
+object Outside extends App with Config with ProducerSettings {
+
+  implicit val system = ActorSystem("outside-actor-system")
+  implicit val materializer = ActorMaterializer()
+
+  val numberOfMessages = {
+    if (args.length == 0) {
+      throw new IllegalArgumentException("You need to provide the number of messages as program argument.")
+    }
+    args(0).toInt
+  }
 }
