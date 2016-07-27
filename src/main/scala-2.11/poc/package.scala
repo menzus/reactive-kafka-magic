bac0cc872f8ef6fe22fc8bbd3dac8b2ba025f017
Tamas Polgar <tms.polgar@gmail.com>
2016-07-28 00:01:00 +0100

    Defining topic names

--- src/main/scala-2.11/poc/package.scala
+++ src/main/scala-2.11/poc/package.scala
@@ -1,4 +1,12 @@
+import java.time.LocalDateTime
+
+import akka.kafka.ConsumerMessage.CommittableMessage
+import akka.kafka.ProducerMessage
+import org.apache.kafka.clients.producer.ProducerRecord
 
 package object poc {
 
+  val Incoming = "incoming-4"
+  val Verified = "verified-4"
+  val Unverified = "unverified-4"
 }
