1e2df9f4f85bd8a69bea99c6ee7626c236fc004b
Tamas Polgar <tms.polgar@gmail.com>
2016-07-30 00:01:00 +0100

    Transforming messages

--- src/main/scala-2.11/poc/package.scala
+++ src/main/scala-2.11/poc/package.scala
@@ -9,4 +9,11 @@ package object poc {
   val Incoming = "incoming-4"
   val Verified = "verified-4"
   val Unverified = "unverified-4"
+
+  def asTransformedProducerMessage[A](topic: String)(msg: CommittableMessage[A, String]) = {
+    ProducerMessage.Message(
+      asTransformedProducerRecord[A](topic)(msg.value),
+      msg.committableOffset
+    )
+  }
 }
