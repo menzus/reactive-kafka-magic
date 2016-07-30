3019a84e885b5ea46e6bacc45a73bc7235f76455
Tamas Polgar <tms.polgar@gmail.com>
2016-07-31 00:01:00 +0100

    Verifier implementation

--- src/main/scala-2.11/poc/Verifier.scala
+++ src/main/scala-2.11/poc/Verifier.scala
@@ -1,5 +1,13 @@
 package poc
 
-class Verifier {
+import akka.actor.ActorSystem
+import akka.kafka._
+import akka.kafka.scaladsl._
+import akka.stream.ActorMaterializer
 
+import scala.util.Random
+
+object Verifier extends App with Config with ConsumerSettings with ProducerSettings {
+
+  implicit val system = ActorSystem("verifier-actor-system")
 }
