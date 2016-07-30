c23a3aacad98ca1cd26a16310b908aa9c7a07239
Tamas Polgar <tms.polgar@gmail.com>
2016-07-31 00:01:00 +0100

    Random verification

--- src/main/scala-2.11/poc/Verifier.scala
+++ src/main/scala-2.11/poc/Verifier.scala
@@ -10,4 +10,7 @@ import scala.util.Random
 object Verifier extends App with Config with ConsumerSettings with ProducerSettings {
 
   implicit val system = ActorSystem("verifier-actor-system")
+  implicit val materializer = ActorMaterializer()
+
+  def isVerified(msg: String): Boolean = Random.nextBoolean()
 }
