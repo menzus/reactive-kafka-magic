2cd45f33200e5622640fb739ed897e7f8f04a422
Tamas Polgar <tms.polgar@gmail.com>
2016-07-31 00:01:00 +0100

    Producer settings finish

--- src/main/scala-2.11/poc/ProducerSettings.scala
+++ src/main/scala-2.11/poc/ProducerSettings.scala
@@ -1,5 +1,12 @@
 package poc
 
-class ProducerSettings {
+import akka.actor.ActorSystem
+import akka.kafka.{ProducerSettings => KProducerSettings}
+import org.apache.kafka.common.serialization.{StringSerializer, ByteArraySerializer}
 
+trait ProducerSettings {
+  self: Config =>
+
+  def producerSettings(implicit system: ActorSystem) = KProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+    .withBootstrapServers(bootstrapServers)
 }
